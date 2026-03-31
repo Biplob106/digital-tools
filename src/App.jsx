3615bf7b84aefaf1060493e43e3382c3e56c789e
Biplob106 <biplobsd106@gmail.com>
2026-03-31 20:15:53 +0600

    load json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-
+import { useEffect, useState } from "react";
 import Nav from './component/Nav.jsx'
 import Banner from './component/Banner.jsx'
+import Tools from './component/Tools.jsx'
+
+
 
 function App() {
+  const [tools, setTools] = useState([]);
+  useEffect(() => {
+    fetch("/tool.json")
+      .then(res => res.json())
+      .then(data => setTools(data));
+  }, []);
+
+  
+
 
   return (
     <>
-      <Nav/>
-      <Banner/>
+      <Nav />
+      <Banner />
+      <Tools tools={tools} />
 
     </>
   )
